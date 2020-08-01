9c11776cf82ab07f0069a8745fab92e9339dcba0
d-yokoi <daiki.kouu@gmail.com>
2020-08-01 23:49:11 +0900

    feat: add function

---
+++ src/function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_big(int number);
+
+int main() {
+  int array[] = { 1, 11, 2, 22, 3, 33 };
+
+  for (int i = 0; i < 6; i++) {
+    print_big(array[i]);
+  }
+
+  return 0;
+}
+
+void print_big(int number) {
+  if (10 < number) {
+    printf("%d is big\n", number);
+  }
+}
